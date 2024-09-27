5325fd4160ff79b77720f9b3f7aa1012f333421d
DAVID <daviff198833@gmail.com>
2024-09-27 09:06:04 +0200

    Primer commit

--- Ejercicio_3.java
+++ Ejercicio_3.java
@@ -1,2 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class Ejercicio_3 {
-}
+
+        public static void main(String[] args) {
+            Set<String> frase = new HashSet<>();
+            frase.add("");
+            frase.add("");
+            frase.add("");
+            frase.add("");
+            frase.add("");
+            frase.add("");
+            frase.add("");
+            frase.add("");
+
+
+            // Recorrer el conjunto
+            for (String palabras : frase) {
+                System.out.println(palabras);
+            }
+        }
+    }
+
