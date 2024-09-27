d7226a324dccbdba38f3d0f555db6f919fab31e9
DAVID <daviff198833@gmail.com>
2024-09-27 09:10:43 +0200

    Segundo commit

--- Ejercicio_3.java
+++ Ejercicio_3.java
@@ -6,14 +6,23 @@ public class Ejercicio_3 {
 
         public static void main(String[] args) {
             Set<String> frase = new HashSet<>();
-            frase.add("");
-            frase.add("");
-            frase.add("");
-            frase.add("");
-            frase.add("");
-            frase.add("");
-            frase.add("");
-            frase.add("");
+            frase.add("Hola,");
+            frase.add("mi");
+            frase.add("nombre");
+            frase.add("es");
+            frase.add("David");
+            frase.add("Fuentes");
+            frase.add("Fernández,");
+            frase.add("vivo");
+            frase.add("en");
+            frase.add("Andújar");
+            frase.add("(Jaén),");
+            frase.add("y");
+            frase.add("tengo");
+            frase.add("36");
+            frase.add("años.");
+            frase.add("Un");
+            frase.add("saludo.");
 
 
             // Recorrer el conjunto
